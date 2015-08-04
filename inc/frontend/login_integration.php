cdfe5b3fe0112f25b95e470b04b56cd0c03b0822
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 06:22:03 +0000

    * Bug fixing for the social login issues with facebook. * Implemented the new version of the facebook php sdk v5.0
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-social-login-lite/tags/1.0.7@1212333 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/frontend/login_integration.php
@@ -0,0 +1,24 @@
+<?php defined('ABSPATH') or die("No script kiddies please!"); ?>
+<?php $options = get_option( APSL_SETTINGS ); ?>
+<div class='apsl-login-networks theme-<?php echo $options['apsl_icon_theme']; ?> clearfix'>
+	<span class='apsl-login-new-text'><?php echo $options['apsl_title_text_field']; ?></span>	
+	<?php if( isset($_REQUEST['error']) || isset($_REQUEST['denied']) ){ ?> 
+	<div class='apsl-error'>
+		<?php _e('You have Access Denied. Please authorize the app to login.', APSL_TEXT_DOMAIN ); ?>
+	</div>
+	<?php } ?>
+
+	<div class='social-networks'>
+	<?php foreach($options['network_ordering'] as $key=>$value): ?>
+	<?php	if($options["apsl_{$value}_settings"]["apsl_{$value}_enable"]==='enable'){ ?>
+	 <a href="?apsl_login_id=<?php echo $value; ?>_login" title='<?php _e('Login with', APSL_TEXT_DOMAIN ); echo ' '.$value; ?>' >
+	 <div class="apsl-icon-block icon-<?php echo $value; ?> clearfix">
+		<i class="fa fa-<?php echo $value; ?>"></i>
+		<span class="apsl-login-text"><?php _e('Login', APSL_TEXT_DOMAIN ); ?></span>
+		<span class="apsl-long-login-text"><?php _e('Login with', APSL_TEXT_DOMAIN ); ?><?php echo ' '.$value; ?></span>
+	 </div>
+	 </a>
+		<?php } ?>
+	<?php endforeach; ?>
+	</div>
+</div>
